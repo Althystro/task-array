37a35a14ac5978c0ab86e60e1c1207326434bf36
Abdulwahab-Alawadhi <abdulwahab.alawadhi56@gmail.com>
2024-11-04 08:32:42 +0300

    done

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,39 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+int[]temps = {50,20,39,10,46};
+
+        temps[2] = 12;
+        System.out.println(temps[2]+"\n_____________"); // This prints the first element
+
+
+
+        for (int temp : temps) {
+            System.out.println(temp);
+        }
+//task 1 Done
+
+
+        List<String> movies = new ArrayList<>();
+        movies.add("Intersteller");
+        movies.add("Tenet");
+        movies.add("Oppenhimer");
+        movies.remove(2);
+        System.out.println(movies);
+
+        for (String movie : movies) {
+            System.out.println(movie);
+        }
+
+
+
+
+
+
+
+
     }
 }
